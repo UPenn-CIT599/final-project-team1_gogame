6dad13f10fa2215a54c0ea953674af602de94ce8
chartung <chartung@seas.upenn.edu>
2019-12-06 06:36:31 -0700

    Fixed timer bug

--- src/Player.java
+++ src/Player.java
@@ -175,7 +175,9 @@ public class Player {
 			if (count == maxAttempts) {
 			    pass();
 			    game.nextPlayersTurn();
-			    game.getTimer().resetByoYomi();
+			    if (game.getTimer() != null) {
+				game.getTimer().resetByoYomi();
+			    }
 			}
 
 			game.getGui().drawBoard();
